4fe7d70e64f7aea5b8425ff42a38693b9d9f1eb0
Daniel Broadbent <dbroadbent85@gmail.com>
2016-09-02 08:05:47 -0600

    Finished part 3, implemented create new song feature

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -4,9 +4,9 @@ RSpec.feature "User creates a song" do
   scenario "they see the song page" do
     artist = create(:artist)
     song_title = "One Love"
-    
+
     visit artist_path(artist)
-    click_on "New song"
+    click_on "New Song"
     fill_in "song_title", with: song_title
     click_on "Create Song"
     expect(page).to have_content song_title
