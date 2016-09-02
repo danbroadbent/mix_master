f7048cbde876ceca732466741893698c8be8539c
Daniel Broadbent <dbroadbent85@gmail.com>
2016-09-02 08:26:46 -0600

    Added song title validation

--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -11,7 +11,14 @@ RSpec.feature "User creates a song" do
     click_on "Create Song"
     expect(page).to have_content song_title
     expect(page).to have_link artist.name, href: artist_path(artist)
+  end
 
+  scenario "validates song title is entered" do
+    artist = create(:artist)
 
+    visit artist_path(artist)
+    click_on "New Song"
+    click_on "Create Song"
+    expect(page).to have_content "Title can't be blank"
   end
 end
